83ef1a486cdf043e6913ead3703bfb40b3c35889
AlexC <AlexC@blackwoodgroup.org.uk>
2018-01-03 14:08:55 +0000

    Added method for PayrollCodeMap, changed to expression-bodied methods

--- Controllers/PayrollController.cs
+++ Controllers/PayrollController.cs
@@ -6,6 +6,7 @@ namespace Blackwood.Access.Controllers
     using Microsoft.Extensions.Logging;
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     [Route("api/[Controller]")]
 	public class PayrollController : ControllerBase
@@ -25,74 +26,49 @@ namespace Blackwood.Access.Controllers
 		}
 
 		[HttpGet("[action]")]
-		public IEnumerable<Team> Teams()
-		{
-            return _dataService.GetTeams();
-		}
+		public IEnumerable<Team> Teams() => _dataService.GetTeams();
 
 		[HttpGet("[action]")]
-		public IEnumerable<Carer> CarersByTeam(int TeamCode, DateTime periodStart)
-		{
-            return _dataService.GetCarersByTeam(TeamCode, periodStart);
-		}
+		public IEnumerable<Carer> CarersByTeam(int TeamCode, DateTime periodStart) => _dataService.GetCarersByTeam(TeamCode, periodStart);
 
 		[HttpGet("[action]")]
-		public Timesheet Timesheet(int carerCode, DateTime weekCommencing)
-		{
-			return _service.GetTimesheet(carerCode, weekCommencing);
-		}
+		public Timesheet Timesheet(int carerCode, DateTime weekCommencing) => _service.GetTimesheet(carerCode, weekCommencing);
 
 		[HttpGet("[action]")]
-		public IEnumerable<Summary> Summaries(int teamCode, DateTime periodStart, DateTime periodEnd){
-			return _service.GetAdjustedSummaries(teamCode, periodStart, periodEnd);
-		}
+		public IEnumerable<Summary> Summaries(int teamCode, DateTime periodStart, DateTime periodEnd)
+            => _service.GetAdjustedSummaries(teamCode, periodStart, periodEnd);
 
 		[HttpPut("[action]")]
 		public Adjustment AddTimesheetAdjustment([FromBody] Adjustment adj)
-		{
-			return _service.PutTimesheetAdjustment(adj, HttpContext.User);
-		}
+		    => _service.PutTimesheetAdjustment(adj, HttpContext.User);
 
 		[HttpDelete("[action]")]
 		public void RemoveTimesheetAdjustment(int id)
-		{
-			_dataService.RemoveTimesheetAdjustment(id);
-		}
+		    => _dataService.RemoveTimesheetAdjustment(id);
 
 		[HttpPut("[action]")]
 		public void UpdateTimesheetAdjustment([FromBody] Adjustment adj)
-		{
-			_service.PutTimesheetAdjustment(adj, HttpContext.User);	// Don't need to return the ID for updates
-		}
+		    => _service.PutTimesheetAdjustment(adj, HttpContext.User);	// Don't need to return the ID for updates
 
 		[HttpGet("[action]")]
 		public IEnumerable<Adjustment> GetTimesheetAdjustmentsByTeam(int teamCode, DateTime periodStart, DateTime periodEnd)
-		{
-			return _dataService.GetTimesheetAdjustmentsByTeam(teamCode, periodStart, periodEnd);
-		}
+		    => _dataService.GetTimesheetAdjustmentsByTeam(teamCode, periodStart, periodEnd);
 
         [HttpGet("[action]")]
         public IEnumerable<Payroll> GetPayrollData(int teamCode, DateTime periodStart, DateTime periodFinish)
-        {
-            return _service.GetPayrollData(teamCode, periodStart, periodFinish);
-        }
+            =>_service.GetPayrollData(teamCode, periodStart, periodFinish);
 
         [HttpGet("[action]")]
 		public ValidationResult Validate(int teamCode, DateTime periodStart, DateTime periodFinish)
-		{
-			return _validation.Validate(teamCode, periodStart, periodFinish);
-		}
+		    => _validation.Validate(teamCode, periodStart, periodFinish);
 
         [HttpGet("[action]")]
-        public Carer GetCarerByCode(int carerCode)
-        {
-            return _dataService.GetCarerByCode(carerCode);
-        }
+        public Carer GetCarerByCode(int carerCode) => _dataService.GetCarerByCode(carerCode);
 
         [HttpGet("[action]")]
-        public WorkPattern WorkPatterns(int carer)
-        {
-            return _dataService.WorkPattern(carer);
-        }
+        public WorkPattern WorkPatterns(int carer) => _dataService.WorkPattern(carer);
+
+		[HttpGet("[action]")]
+		public List<PayrollCodeMap> CodeMap() => _dataService.GetPayrollCodeMap().ToList();
 	}
 }
