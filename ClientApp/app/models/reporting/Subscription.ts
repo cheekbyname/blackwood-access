a3bc975cc9268a4dfbbebdf8dd196a6e871ee335
Alex Cheek <AlexC@blackwoodgroup.org.uk>
2018-08-03 17:18:23 +0100

    Added AccessUser to Subscription model

--- ClientApp/app/models/reporting/Subscription.ts
+++ ClientApp/app/models/reporting/Subscription.ts
@@ -1,9 +1,12 @@
 import { Schedule } from "./Schedule";
+import { AccessUser } from "../AccessUser";
 
 export class Subscription {
     public id: number;
     public scheduleId: number;
+    public accessUserId: number;
     public emailAddress: string;
 
     public schedule: Schedule;
+    public accessUser: AccessUser;
 }
