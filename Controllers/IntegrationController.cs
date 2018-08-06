9f203aa8e567b54c2e7ab63bf3faa58093fe6695
Alex Cheek <AlexC@blackwoodgroup.org.uk>
2018-08-06 17:01:51 +0100

    Added scaffold for Integration module Controller

---
+++ Controllers/IntegrationController.cs
@@ -0,0 +1,21 @@
+﻿namespace Blackwood.Access.Controllers
+{
+    using Core.Operations.Integration;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IntegrationController : ControllerBase
+    {
+        private readonly ILogger<IntegrationController> _logger;
+        private readonly IIntegrationData _integrationData;
+
+        public IntegrationController(ILogger<IntegrationController> logger, IIntegrationData integrationData)
+            => (_logger, _integrationData) = (logger, integrationData);
+
+        [Route("[action]")]
+        public async Task<IActionResult> AllUsers() => Ok(await _integrationData.GetAllUsers());
+    }
+}
