0533d4a27e15154ab1982e3c900fb693fef99c0b
Alex Cheek <AlexC@blackwoodgroup.org.uk>
2018-08-14 10:29:26 +0100

    Added filter performance tweaks

--- ClientApp/app/integration/user.integration.component/user.integration.component.ts
+++ ClientApp/app/integration/user.integration.component/user.integration.component.ts
@@ -5,58 +5,71 @@ import { User } from "../../models/integration/User";
 import { Utils } from "../../Utils";
 
 import { IntegrationProvider } from "../integration.provider";
-import { BehaviorSubject, Observable } from "../../../../node_modules/rxjs";
+import { BehaviorSubject } from "rxjs";
 
 @Component({
-    selector: 'user-integration',
-    templateUrl: './user.integration.component.html',
-    styleUrls: ['./user.integration.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+	selector: "user-integration",
+	templateUrl: "./user.integration.component.html",
+	styleUrls: ["./user.integration.component.css"],
+	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserIntegrationComponent implements AfterViewInit {
-    constructor(private ip: IntegrationProvider, private cd: ChangeDetectorRef) {
-        ip.integrationUsers$.subscribe(au => {
-            this.users = au;
-            this.filteredUsers = this.users;
-            this.cd.detectChanges();
-        });
+	constructor(private ip: IntegrationProvider, private cd: ChangeDetectorRef) {
+		ip.integrationUsers$.subscribe(au => {
+			this.users = au;
+			this.filteredUsers = this.users;
+			this.cd.detectChanges();
+		});
 
-        /* Filter users on searchbox contents */
-        this.searchTerm$.subscribe(term => {
-            if (this.users == null) return;
-            term = term.toLowerCase();
-            this.filteredUsers = this.users.filter(user => (user.firstName + ' ' + user.lastName).toLowerCase().includes(term));
-            this.cd.detectChanges();
-        });
-    }
+		/* Filter users on searchbox contents */
+		this.searchTerm$ //.switchMap(term => term)
+			.subscribe(term => {
+				if (this.users == null) {
+					this.filteredUsers = this.users;
+				} else {
+					term = term.toLowerCase();
+					this.filteredUsers = this.users.filter(user =>
+						(user.firstName + " " + user.lastName).toLowerCase().includes(term)
+					);
+				}
+				this.filtering = false;
+				this.cd.detectChanges();
+			});
+	}
 
-    ngAfterViewInit() {
-        this.cd.detach();
-    }
+	ngAfterViewInit() {
+		this.cd.detach();
+	}
 
-    private _searchTerm: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+	private _searchTerm: BehaviorSubject<string> = new BehaviorSubject<string>(
+		null
+	);
 
-    private searchTerm$ = this._searchTerm.asObservable().delay(200)
-        .filter(term => term != null).debounceTime(200).distinctUntilChanged();
+	private searchTerm$ = this._searchTerm.asObservable()
+		.delay(200).debounceTime(200);
 
-    public users: User[];
-    public filteredUsers: User[];
-    public Utils = Utils;
+	public users: User[];
+	public filteredUsers: User[];
+	public Utils = Utils;
+	public filtering: boolean = false;
 
-    public roleDesc(role: string): string {
-        switch (role) {
-            case 'spt': return 'Staff';
-            case 'usr': return 'Client';
-        }
-    }
+	public roleDesc(role: string): string {
+		switch (role) {
+			case "spt":
+				return "Staff";
+			case "usr":
+				return "Client";
+		}
+	}
 
-    public onSearchChanged(term: string) {
-        this.filteredUsers = null;
-        this.cd.detectChanges();
-        this._searchTerm.next(term);
-    }
+	public onSearchChanged(term: string) {
+		this.filtering = true;
+		this.filteredUsers = null;
+		this.cd.detectChanges();
+		this._searchTerm.next(term);
+	}
 
-    public trackByFn(index, item: User) {
-        return item.personCode;
-    }
+	public trackByFn(index, item: User) {
+		return item.personCode;
+	}
 }
