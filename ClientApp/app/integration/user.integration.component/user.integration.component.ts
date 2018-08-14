1e189c9f779abd0ee13a812890104c14af7628ec
Alex Cheek <AlexC@blackwoodgroup.org.uk>
2018-08-14 15:55:19 +0100

    Add search term as route param for User Integration component

--- ClientApp/app/integration/user.integration.component/user.integration.component.ts
+++ ClientApp/app/integration/user.integration.component/user.integration.component.ts
@@ -1,11 +1,13 @@
 import { Component, ChangeDetectionStrategy, ChangeDetectorRef, AfterViewInit } from "@angular/core";
+import { Location } from "@angular/common";
+import { ActivatedRoute } from "@angular/router";
 
 import { User } from "../../models/integration/User";
 
 import { Utils } from "../../Utils";
 
 import { IntegrationProvider } from "../integration.provider";
-import { BehaviorSubject, Observable, ObjectUnsubscribedError } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 
 @Component({
 	selector: "user-integration",
@@ -14,15 +16,27 @@ import { BehaviorSubject, Observable, ObjectUnsubscribedError } from "rxjs";
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserIntegrationComponent implements AfterViewInit {
-	constructor(private ip: IntegrationProvider, private cd: ChangeDetectorRef) {
-		ip.integrationUsers$.subscribe(au => {
-			this.users = au;
-			this._filteredUsers.next(au);
-			this.cd.detectChanges();
-		});
+	constructor(private ip: IntegrationProvider, private cd: ChangeDetectorRef, private route: ActivatedRoute,
+		private loc: Location) { }
+
+	ngAfterViewInit() {
+		this.cd.detach();
+
+		this.ip.integrationUsers$.subscribe(au => {this.users = au });
+
+		Observable.combineLatest(this.ip.integrationUsers$, this.route.queryParams, (u, p) => { return { "users": u, "params": p } })
+			.filter(x => x.users != null)
+			.subscribe(x => {
+				if (x.params['search'] != undefined) {
+					this.searchTerm = x.params['search'];
+				}
+				this.onSearchChanged(this.searchTerm);
+			});
 
 		this.searchTerm$
-			.switchMap(term => this.filterUsers(term))
+			.switchMap(term => {
+				return this.filterUsers(term);
+			})
 			.subscribe(users => {
 				this._filteredUsers.next(users);
 
@@ -31,11 +45,7 @@ export class UserIntegrationComponent implements AfterViewInit {
 			});
 	}
 
-	ngAfterViewInit() {
-		this.cd.detach();
-	}
-
-	private _searchTerm: BehaviorSubject<string> = new BehaviorSubject<string>(null);
+	private _searchTerm: BehaviorSubject<string> = new BehaviorSubject<string>('');
 	private _filteredUsers: BehaviorSubject<User[]> = new BehaviorSubject<User[]>(null);
 
 	private searchTerm$ = this._searchTerm.debounceTime(250);
@@ -43,6 +53,7 @@ export class UserIntegrationComponent implements AfterViewInit {
 
 	public users: User[];
 	public Utils = Utils;
+	public searchTerm: string = '';
 	public filtering: boolean = false;
 
 	public roleDesc(role: string): string {
@@ -56,15 +67,19 @@ export class UserIntegrationComponent implements AfterViewInit {
 
 	public onSearchChanged(term: string) {
 		// Clear out list and notify user
+		if (term != '') this.loc.go('integration/users', `search=${term}`);
+
 		this.filtering = true;
 		this._filteredUsers.next(null);
 		this.cd.detectChanges();
-
 		this._searchTerm.next(term);
 	}
 
 	private filterUsers(term: string): Observable<User[]> {
-		if (this.users == null) return Observable.of(null);
+		// Trap nulls
+		if (this.users == null || term == null) return Observable.of([]);
+
+		// Filter on lowercase
 		term = term.toLowerCase();
 		var filteredUsers = this.users.filter(user =>
 			(user.firstName + ' ' + user.lastName).toLowerCase().includes(term)
